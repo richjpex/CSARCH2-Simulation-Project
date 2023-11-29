cabc16b6dd8ce40415aece1780f63feb65d7ca88
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2023-11-29 11:54:11 +0800

    Radiobuttons for Test Casing

--- src/GUI.py
+++ src/GUI.py
@@ -15,18 +15,28 @@ class CacheSimulatorGUI:
         self.memory_block_label.grid(row=2, column=0, padx=5, pady=5, sticky="e")
         self.memory_block_entry.grid(row=2, column=1, padx=5, pady=5)
 
+
+        self.test_case_label = ttk.Label(root, text="Test Cases:")
+        self.test_case_values = ['Default', 'Sequential', 'Random', 'Mid-Repeating']
+        self.selected_test_case = tk.StringVar(value=self.test_case_values[0])  # Set default value
+        for i, test_case in enumerate(self.test_case_values):
+            row_position = i // 2 + 3  # Start from row 3 and alternate every 2 radio buttons
+            col_position = i % 2
+            radio_button = ttk.Radiobutton(root, text=test_case, variable=self.selected_test_case, value=test_case)
+            radio_button.grid(row=row_position, column=col_position, padx=5, pady=5)
+        
         # Checkbox for step-by-step tracing
         self.step_by_step_var = tk.IntVar()
         self.step_by_step_checkbutton = ttk.Checkbutton(
             self.root, text="Step-by-Step Tracing", variable=self.step_by_step_var
         )
-        self.step_by_step_checkbutton.grid(row=3, column=0, columnspan=2, pady=5)
+        self.step_by_step_checkbutton.grid(row=5, column=0, columnspan=2, pady=5)
 
         # Button to start cache simulation
         self.start_button = ttk.Button(
             self.root, text="Simulate Cache", command=self.start_simulation
         )
-        self.start_button.grid(row=4, column=0, columnspan=2, pady=10)
+        self.start_button.grid(row=6, column=0, columnspan=2, pady=10)
 
         # Cache simulator instance
         self.cache_simulator = None
@@ -44,12 +54,12 @@ class CacheSimulatorGUI:
         ]
         for i, label_text in enumerate(labels_text):
             label = ttk.Label(self.root, text=label_text)
-            label.grid(row=i + 5, column=0, padx=5, pady=5, sticky="e")
+            label.grid(row=i + 7, column=0, padx=5, pady=5, sticky="e")
             self.output_labels.append(label)
 
         # Text area for cache simulation log
         self.text_log = tk.Text(self.root, height=10, width=40, state=tk.DISABLED)
-        self.text_log.grid(row=12, column=0, columnspan=2, pady=10)
+        self.text_log.grid(row=13, column=0, columnspan=2, pady=10)
 
         # Configure grid weights
         self.root.columnconfigure(0, weight=1)
@@ -63,35 +73,44 @@ class CacheSimulatorGUI:
 
     def start_simulation(self):
         try:
+
+            selected_test_case = self.selected_test_case.get()
+            
+            if selected_test_case == 'Default':
             # Get the number of memory blocks from the user input
-            memory_block = int(self.memory_block_entry.get())
-            self.memory_blocks = memory_block
+                memory_block = int(self.memory_block_entry.get())
+                self.memory_blocks = memory_block
 
-            # Initialize the cache simulator
-            self.cache_simulator = CacheSimulator()
-            self.cache_blocks = 12
+                # Initialize the cache simulator
+                self.cache_simulator = CacheSimulator()
+                self.cache_blocks = 32 # the default 
 
-            # Generate a random memory access sequence
-            memory_access_sequence = [
-                random.randint(0, 2 * (memory_block // 2) - 1) for _ in range(4 * (memory_block // 2))
-            ]
-            self.memory_access_sequence = memory_access_sequence
+                # Generate a random memory access sequence
+                memory_access_sequence = [
+                    random.randint(0, 2 * (memory_block // 2) - 1) for _ in range(4 * (memory_block // 2))
+                ]
+                self.memory_access_sequence = memory_access_sequence
 
-            # Run the cache simulation
-            results = self.cache_simulator.run_simulation(memory_access_sequence)
+                # Run the cache simulation
+                results = self.cache_simulator.run_simulation(memory_access_sequence)
 
-            # Update the GUI labels with simulation results
-            self.output_labels[0]["text"] = f"Memory Access Count: {results['hits'] + results['misses']}"
-            self.output_labels[1]["text"] = f"Cache Hit Count: {results['hits']}"
-            self.output_labels[2]["text"] = f"Cache Miss Count: {results['misses']}\n"
-            self.output_labels[3]["text"] = f"Cache Hit Rate: {results['cache_hit_rate']:.2%}"
-            self.output_labels[4]["text"] = f"Cache Miss Rate: {results['cache_miss_rate']:.2%}"
-            self.output_labels[5]["text"] = f"Average Memory Access Time: {results['average_memory_access_time']:.2f}"
-            self.output_labels[6]["text"] = f"Total Memory Access Time: {results['total_memory_access_time']:.2f}"
+                # Update the GUI labels with simulation results
+                self.output_labels[0]["text"] = f"Memory Access Count: {results['hits'] + results['misses']}"
+                self.output_labels[1]["text"] = f"Cache Hit Count: {results['hits']}"
+                self.output_labels[2]["text"] = f"Cache Miss Count: {results['misses']}\n"
+                self.output_labels[3]["text"] = f"Cache Hit Rate: {results['cache_hit_rate']:.2%}"
+                self.output_labels[4]["text"] = f"Cache Miss Rate: {results['cache_miss_rate']:.2%}"
+                self.output_labels[5]["text"] = f"Average Memory Access Time: {results['average_memory_access_time']:.2f}"
+                self.output_labels[6]["text"] = f"Total Memory Access Time: {results['total_memory_access_time']:.2f}"
 
-            # Display the cache simulation log
-            self.display_cache_log()
-
+                # Display the cache simulation log
+                self.display_cache_log()
+            elif selected_test_case == 'Sequential':
+                pass
+            elif selected_test_case == 'Random':
+                pass
+            elif selected_test_case == 'Mid-Repeating':
+                pass
         except ValueError as e:
             # Handle the case where the user enters invalid input
             pass
